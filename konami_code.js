af9aeffec321b6b465f352c44d17f941b9c71837
tunde Oretade <tundeiness@yahoo.co.uk>
2018-10-01 19:46:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,12 +15,19 @@ let index = 0;
 
  function init() {
 document.body.addEventListener("keydown", function(e) {
+  
   const key = e.key;
- console.log(key);
+  
+// console.log(key);
+
   if (key === codes[index]) {
+    
     index++;
-    console.log(index);
+    
+   // console.log(index);
+   
     if (index === codes.length) {
+      
       alert("Hurray!");
 
       index = 0;
@@ -32,19 +39,3 @@ document.body.addEventListener("keydown", function(e) {
  }
 
 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
- console.log(key);
-  if (key === codes[index]) {
-    index++;
-    console.log(index);
-    if (index === codes.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
